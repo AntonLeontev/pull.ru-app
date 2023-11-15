463fbcbbbc7b4d0e3bc68ca64fb1a0db90ee2760
Anton <aner-anton@yandex.ru>
2023-11-15 17:57:02 +0300

    delivery webhook

---
+++ deploy.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Config
+
+set('repository', 'https://github.com/AntonLeontev/pull.ru-app.git');
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+
+host('45.146.165.254')
+    ->set('remote_user', 'deployer')
+    ->set('deploy_path', '~/pull.ru-app');
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock');
