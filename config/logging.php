613baef902da78807229a02db4220556990b07a2
Anton <aner-anton@yandex.ru>
2023-12-05 15:39:34 +0300

    logging file change

--- config/logging.php
+++ config/logging.php
@@ -60,7 +60,7 @@ return [
 
         'single' => [
             'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
+            'path' => storage_path('logs/laravel'.now()->format('Y-m-d').'.log'),
             'level' => env('LOG_LEVEL', 'debug'),
             'replace_placeholders' => true,
         ],
