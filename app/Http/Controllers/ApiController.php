01c5eaff73cca96a1983477747a64d60cd898385
Anton <aner-anton@yandex.ru>
2023-12-29 15:27:41 +0300

    fix: api for allowed regions

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -6,6 +6,20 @@ class ApiController extends Controller
 {
     public function allowedRegions()
     {
-        return response()->json(array_keys(config('delivery.cdek.allowed_regions')));
+        $regions = array_keys(config('delivery.cdek.allowed_regions'));
+        if ($key = array_search('Татарстан', $regions)) {
+            $regions[$key] = 'Республика Татарстан';
+        }
+        if ($key = array_search('Удмуртия', $regions)) {
+            $regions[$key] = 'Республика Удмуртия';
+        }
+        if ($key = array_search('Марий Эл', $regions)) {
+            $regions[$key] = 'Республика Марий Эл';
+        }
+        if ($key = array_search('Мордовия', $regions)) {
+            $regions[$key] = 'Республика Мордовия';
+        }
+
+        return response()->json($regions);
     }
 }
