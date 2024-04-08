678e9bf24f74d2f2796faf332821ffd67b391881
Anton <aner-anton@yandex.ru>
2024-04-08 22:43:04 +0300

    add: exception handling on demand creating

--- src/Domain/Synchronizer/Jobs/CreateDemandInMS.php
+++ src/Domain/Synchronizer/Jobs/CreateDemandInMS.php
@@ -7,11 +7,13 @@ use App\Services\MoySklad\Entities\Counterparty;
 use App\Services\MoySklad\Entities\CustomerOrder;
 use App\Services\MoySklad\Entities\Variant as MSVariant;
 use App\Services\MoySklad\MSApiService;
+use Exception;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 use Src\Domain\Synchronizer\Models\Client;
 use Src\Domain\Synchronizer\Models\Order;
 use Src\Domain\Synchronizer\Models\Variant;
@@ -26,26 +28,32 @@ class CreateDemandInMS implements ShouldQueue
 
     public function handle(): void
     {
-        $insalesOrder = InSalesApi::getOrder($this->order->insales_id);
-        $clientInsalesId = $insalesOrder->json('client.id');
-        $client = Client::where('insales_id', $clientInsalesId)->first();
-        $counterparty = Counterparty::make($client->moy_sklad_id);
+        try {
+            $insalesOrder = InSalesApi::getOrder($this->order->insales_id);
+            $clientInsalesId = $insalesOrder->json('client.id');
+            $client = Client::where('insales_id', $clientInsalesId)->first();
+            $counterparty = Counterparty::make($client->moy_sklad_id);
 
-        $customerOrder = CustomerOrder::make($this->order->moy_sklad_id);
-        $positions = [];
+            $customerOrder = CustomerOrder::make($this->order->moy_sklad_id);
+            $positions = [];
 
-        foreach ($insalesOrder->json('order_lines') as $line) {
-            $position = [];
-            $variant = Variant::where('insales_id', $line['variant_id'])->first();
+            foreach ($insalesOrder->json('order_lines') as $line) {
+                $position = [];
+                $variant = Variant::where('insales_id', $line['variant_id'])->first();
 
-            $position['quantity'] = $line['quantity'];
-            $position['price'] = $line['full_sale_price'] * 100;
-            $position['assortment'] = MSVariant::make($variant->moy_sklad_id);
-            $position['reserve'] = $line['reserved_quantity'];
+                $position['quantity'] = $line['quantity'];
+                $position['price'] = $line['full_sale_price'] * 100;
+                $position['assortment'] = MSVariant::make($variant->moy_sklad_id);
+                $position['reserve'] = $line['reserved_quantity'];
 
-            $positions[] = $position;
+                $positions[] = $position;
+            }
+
+            MSApiService::createDemandFromDeliveredOrder($counterparty, $customerOrder, $positions);
+        } catch (Exception $e) {
+            Log::channel('telegram')
+                ->error("Ошибка при создании отгрузки в МС.\nЗаказ: ".$this->order->number.'. Ошибка: '.$e->getMessage());
+            $this->fail($e);
         }
-
-        MSApiService::createDemandFromDeliveredOrder($counterparty, $customerOrder, $positions);
     }
 }
