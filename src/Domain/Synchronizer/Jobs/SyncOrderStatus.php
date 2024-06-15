8342aa8f5916b5e2118772211f6ee422bc402117
Anton <aner-anton@yandex.ru>
2024-06-15 16:15:51 +0300

    add: handle status update job errors

--- src/Domain/Synchronizer/Jobs/SyncOrderStatus.php
+++ src/Domain/Synchronizer/Jobs/SyncOrderStatus.php
@@ -7,6 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 use Src\Domain\Synchronizer\Actions\SyncOrderStatusFromCdek;
 use Src\Domain\Synchronizer\Models\Order;
 
@@ -20,6 +21,11 @@ class SyncOrderStatus implements ShouldQueue
 
     public function handle(SyncOrderStatusFromCdek $action): void
     {
-        $action->handle($this->order);
+        try {
+            $action->handle($this->order);
+        } catch (\Throwable $th) {
+            Log::channel('telegram')->warning('Job Error: Не удалось обновить статус заказа '.$this->order->number.'. '.$th->getMessage());
+            $this->fail();
+        }
     }
 }
